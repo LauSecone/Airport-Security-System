6a2bcc2105b56a087f51f0c306eff2e4f2a0dd7f
wddcbfree <wddcbfree@163.com>
2017-06-01 23:13:05 +0800

    Add files via upload

--- allocust.cpp
+++ allocust.cpp
@@ -5,25 +5,24 @@ using namespace std;
 
 void allocust(int &quenum) {
 	int i, custtotal = 0, custneed = 0;
-	bool winstate[MAX_WINDOWS] = { 0 };
-	for (i = 1; i <= REAL_WINDOWS; i++) {
-		if (windows[i].State != 4 && windows[i].State != 5 && windows[i].RestSignal == 0)
+	bool winstate[10] = {0};
+	for (i = 1; i <= REAL_WINDOWS; ++i)
+		if (windows[i].State != RESTTING_PORT && windows[i].State != CLOSE_PORT)
 		{
 			winstate[i] = 1;
 			custtotal += windows[i].CurNum;//统计安检口乘客总数 
 			custneed += MaxSeqLen;//统计安检口填满所需乘客数 
 		}
-	}
 	while (custtotal < custneed && quenum > 0) {
 		int mincust = 2100000000, dest;
 		double maxeffi = -1;
-		for (i = 1; i <= REAL_WINDOWS; i++)
+		for (i = 1; i <= REAL_WINDOWS; ++i)
 			if (winstate[i] && windows[i].CurNum < mincust)
-				mincust = windows[i].CurNum;
-		for (i = 1; i <= REAL_WINDOWS; i++)
+		  		mincust = windows[i].CurNum;
+		for (i = 1; i <= REAL_WINDOWS; ++i)
 			if (winstate[i] && windows[i].CurNum == mincust)
 			{
-				double efficiency = (double)windows[i].TotNum / (windows[i].TotServeTime + 1.0);
+				double efficiency = (double)windows[i].TotNum /(windows[i].TotServeTime + 1);
 				if (efficiency > maxeffi)
 				{
 					maxeffi = efficiency;
@@ -33,6 +32,5 @@ void allocust(int &quenum) {
 		++windows[dest].CurNum;
 		++custtotal;
 		--quenum;
-		//cout << dest << ' ' << windows[dest].CurNum << ' ' << *quenum << endl;
 	}
 }
