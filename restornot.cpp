d81b56b79a28dbdb9416101da5c6352eab367d3b
wddcbfree <wddcbfree@163.com>
2017-06-01 23:14:52 +0800

    Add files via upload

--- restornot.cpp
+++ restornot.cpp
@@ -3,51 +3,48 @@
 
 using namespace std;
 
-static bool reststate[10] = { 0 };
+static bool reststate[10];
 
-void restornot(int &quenum, const string &restrequest) {
-	int i;
+void restornot(int &quenum, string restrequest) {
+	int i, cntrequest = 0;
 	double cnt = 0;
-	int cntrequest = 0;
-	bool requeststore[10] = { 0 };
-	for (i = 1; i <= REAL_WINDOWS; i++) {
-		if (restrequest[i] == 'R')
-			requeststore[i] = 1;
-	}
-	for (i = 1; i <= REAL_WINDOWS; i++) {
-		if (reststate[i] || requeststore[i])
-			cntrequest++;
-	}
-	for (i = 1; i <= REAL_WINDOWS; i++)
-		if (windows[i].State != 4 && windows[i].State != 5)
-			cnt++;
-	while (cnt >= 1 && quenum / cnt < MaxSeqLen  && cntrequest > 0) {
+	bool requeststore[10]={0};
+	for (i = 1; i <= REAL_WINDOWS; ++i)
+	    if(restrequest[i] == 'R')
+	        requeststore[i] = 1; 
+	for (i = 1; i <= REAL_WINDOWS; ++i)
+		if (windows[i].State != RESTTING_PORT && windows[i].State != CLOSE_PORT)
+			++cnt;
+	for (i = 1; i <= REAL_WINDOWS; ++i)
+	    if(reststate[i] || requeststore[i])
+		    ++cntrequest;		
+	while(cnt >= 1 && quenum / cnt < MaxSeqLen && cntrequest > 0){
 		int flag = 1;
-		for (i = 1; i <= REAL_WINDOWS && flag; i++)
-			if (reststate[i]) {
-				flag = 0;
-				reststate[i] = 0;
-				requeststore[i] = 0;
-				windows[i].RestSignal = 1;
-				cnt--;
+		for (i = 1; i <= REAL_WINDOWS && flag; ++i)
+		    if(reststate[i]){
+		    	flag = 0;
+		    	reststate[i] = 0;
+		    	requeststore[i] = 0;
+		    	windows[i].RestSignal = 1;
+		    	--cnt;
 			}
-		if (flag) {
+		if(flag){
 			int sec_flag = 1, minrest = 2100000000;
-			for (i = 1; i <= REAL_WINDOWS; i++)
-				if (requeststore[i] && windows[i].TotOffTime < minrest)
-					minrest = windows[i].TotOffTime;
-			for (i = 1; i <= REAL_WINDOWS && sec_flag; i++) {
-				if (requeststore[i] && windows[i].TotOffTime == minrest) {
-					windows[i].RestSignal = 1;
-					requeststore[i] = 0;
-					sec_flag = 0;
-					cnt--;
+			for (i = 1; i <= REAL_WINDOWS; ++i)
+			    if (requeststore[i] && windows[i].TotOffTime < minrest)
+				    minrest = windows[i].TotOffTime;
+		    for (i = 1; i <= REAL_WINDOWS && sec_flag; ++i){
+			    if (requeststore[i] && windows[i].TotOffTime == minrest){
+				    windows[i].RestSignal = 1;
+				    requeststore[i] = 0;
+				    sec_flag = 0;
+				    --cnt;
 				}
-			}
-			cntrequest--;
+		    }
 		}
+		--cntrequest;	
 	}
 	for (i = 1; i <= REAL_WINDOWS; ++i)
-		if (requeststore[i])
-			reststate[i] = 1;
+	    if(requeststore[i])
+		    reststate[i] = 1;		
 }
