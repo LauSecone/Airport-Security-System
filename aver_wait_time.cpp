fe233f1eb6a37ab05191c1920f21e7e8339a6e80
BuWH <wenhekrait@qq.com>
2017-06-02 23:26:03 +0800

    update

--- aver_wait_time.cpp
+++ aver_wait_time.cpp
@@ -12,17 +12,17 @@ static queue<int> s_CustInLine;
 
 void cust_in(int InNum,int quenum) {
 	int i;
-	if (quenum == 0) {
+	if (quenum == 0) {//若排队区无人（乘客不需排队）
 		++s_TotCustNum;
 		g_AveWaitTime = ceil((double)s_TotWaitTime / s_TotCustNum);
 	}
-	for (i = 0; i <= InNum - 1; ++i) {
+	for (i = 0; i <= InNum - 1; ++i) {//需要排队
 		s_CustInLine.push(g_Time);
 	}
 	return;
 }
 
-void cust_out() {
+void cust_out() {//计算平均时间
 	++s_TotCustNum;
 	s_TotWaitTime += g_Time - s_CustInLine.back();
 	s_CustInLine.pop();
